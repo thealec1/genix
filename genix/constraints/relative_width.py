14cdfaf1dd40b0dc9a60476f13462c58f2912af7
thealec1 <46400863+thealec1@users.noreply.github.com>
2020-08-26 23:44:56 -0400

    Update relative_width.py

--- genix/constraints/relative_width.py
+++ genix/constraints/relative_width.py
@@ -1,5 +1,4 @@
 from genix.constraints.constraint import Constraint
-from genix.components.ui_text import UIText
 from genix.containers.ui_grid import UIGrid
 import pygame as pg
 
@@ -7,6 +6,7 @@ import pygame as pg
 class RelativeWidth(Constraint):
 
     def __init__(self, width_percent):
+        super().__init__()
         self.width_percent = width_percent
 
     def update_constraint(self, ui_element=None):
