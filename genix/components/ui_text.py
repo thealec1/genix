04b652b5054476203ef8ffd0fe1fa21f77dd2ed5
thealec1 <46400863+thealec1@users.noreply.github.com>
2020-08-26 23:56:50 -0400

    Update ui_text.py

--- genix/components/ui_text.py
+++ genix/components/ui_text.py
@@ -1,5 +1,7 @@
-from genix.components.ui_component import UIComponent
 import pygame as pg
+from genix.components.ui_component import UIComponent
+from genix.constraints.relative_width import RelativeWidth
+from genix.constraints.relative_height import RelativeHeight
 
 
 class UIText(UIComponent):
@@ -12,6 +14,8 @@ class UIText(UIComponent):
         self.font = "arial"
         self.bold = False
         self.italic = False
+        self.updated = False
+        self.parent_width, self.parent_height = 0, 0
         self.update_text()
 
     def set_scale(self, scale):
@@ -19,6 +23,7 @@ class UIText(UIComponent):
 
     def set_text(self, text):
         self.text = text
+        self.update_text()
 
     def set_colour(self, colour_value):
         self.colour = colour_value
@@ -40,8 +45,42 @@ class UIText(UIComponent):
         self.font = font
         self.update_text()
 
+    def set_parent_sizes(self, width, height):
+        if not self.updated:
+            self.parent_width, self.parent_height = width, height
+            self.updated = True
+            self.set_constraint_values()
+
+    def set_constraint_values(self):
+        if self.parent_width != 0 and self.parent_height != 0:
+
+            w_percent = self.rect.width / self.parent_width
+            h_percent = self.rect.height / self.parent_height
+
+            # If we found the constraint already added to the list, we modify it's percent
+            # instead of adding a new instance.
+
+            found_element = False
+
+            for constraint in self.constraints:
+
+                index = self.constraints.index(constraint)
+
+                if isinstance(constraint, RelativeWidth):
+                    found_element = True
+                    self.constraints[index].width_percent = w_percent
+
+                if isinstance(constraint, RelativeHeight):
+                    self.constraints[index].height_precent = h_percent
+
+            if not found_element:
+                self.add_constraint(RelativeWidth(w_percent))
+                self.add_constraint(RelativeHeight(h_percent))
+
     def update_text(self):
         font = pg.font.SysFont(self.font, self.size, self.bold, self.italic)
         self.original_image = font.render(str(self.text), 1, self.colour)
-        self.image = self.original_image
+        self.image = self.original_image.copy()
         self.rect = self.image.get_rect()
+        self.set_constraint_values()
+        self.update_constraints()
