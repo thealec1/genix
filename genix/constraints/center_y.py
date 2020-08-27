dc9fabbf754b0c1bea74afbebf4e75588fdfb046
thealec1 <46400863+thealec1@users.noreply.github.com>
2020-08-26 23:40:39 -0400

    Update center_y.py

--- genix/constraints/center_y.py
+++ genix/constraints/center_y.py
@@ -2,7 +2,9 @@ from genix.constraints.constraint import Constraint
 
 
 class CenterY(Constraint):
+    def __init__(self):
+        super().__init__()
+        self.priority = 2
 
     def update_constraint(self, ui_element=None):
-        # ui_element.rect.y += (ui_element.parent_screen.rect.height//2) - (ui_element.image.get_height()//2)
         ui_element.add_y((ui_element.parent_screen.rect.height//2) - (ui_element.image.get_height()//2))
