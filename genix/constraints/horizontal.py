61bb46423e6a3deed3ab38e9af7b8461eea194e5
thealec1 <46400863+thealec1@users.noreply.github.com>
2020-08-26 23:42:26 -0400

    Update horizontal.py

--- genix/constraints/horizontal.py
+++ genix/constraints/horizontal.py
@@ -2,10 +2,10 @@ from genix.constraints.constraint import Constraint
 
 
 class Horizontal(Constraint):
-
     def __init__(self, x_offset):
+        super().__init__()
+        self.priority = 2
         self.x_offset = x_offset
 
     def update_constraint(self, ui_element=None):
-        # ui_element.rect.x += ui_element.parent_screen.rect.width * self.x_offset
         ui_element.add_x(ui_element.parent_screen.rect.width * self.x_offset)
