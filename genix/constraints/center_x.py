58f171c01cb92016af90e30831053c80bf9f25f2
thealec1 <46400863+thealec1@users.noreply.github.com>
2020-08-26 23:40:06 -0400

    Update center_x.py

--- genix/constraints/center_x.py
+++ genix/constraints/center_x.py
@@ -2,7 +2,9 @@ from genix.constraints.constraint import Constraint
 
 
 class CenterX(Constraint):
+    def __init__(self):
+        super().__init__()
+        self.priority = 2
 
     def update_constraint(self, ui_element=None):
-        # ui_element.rect.x += (ui_element.parent_screen.rect.width//2) - (ui_element.image.get_width()//2)
         ui_element.add_x((ui_element.parent_screen.rect.width//2)-(ui_element.image.get_width()//2))
