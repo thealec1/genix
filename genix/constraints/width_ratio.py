bb7d2b22ab3b7aea9036dd580863725fd183b3a3
thealec1 <46400863+thealec1@users.noreply.github.com>
2020-08-26 23:46:12 -0400

    Update width_ratio.py

--- genix/constraints/width_ratio.py
+++ genix/constraints/width_ratio.py
@@ -3,6 +3,9 @@ import pygame as pg
 
 
 class WidthRatio(Constraint):
+    def __init__(self):
+        super().__init__()
+        self.priority = 1
 
     def update_constraint(self, ui_element=None):
         ui_element.rect.width = ui_element.rect.height
